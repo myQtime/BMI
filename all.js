bb51af3b800821d9e3e94fe8543afe7c0998e3d7
myQtime <myqtimelife@gmail.com>
2021-09-05 21:44:09 +0800

    console

--- all.js
+++ all.js
@@ -160,7 +160,6 @@ list.addEventListener('click',function(e){
 
 //enter 輸入
 document.addEventListener("keyup",function(e){
-    console.log(e);
     if (e.key ==="Enter"){
         run.click()
     }
